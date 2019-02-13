978daa42a8e611005f4903c2d5e8df6e6c162237
aurelien <duquenneaurelien@gmail.com>
2019-02-13 22:07:31 +0100

    aaaaaé

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,11 +13,12 @@ before_action :authenticate_user!, only: [:create, :new, :show]
   end
 
   def create
-    event = Event.new(description: params[:description], title: params[:title], location: [:location], admin_id: params[:id])
+    @event = Event.create(description: params[:description], title: params[:title], location: [:location], admin_id: params[:id], duration: params[:duration].to_i, price: params[:price].to_i)
+    @event = current_user
     puts "$"*60
     puts params
     puts "$" * 60
-    if event.save
+    if @event.save
       redirect_to root_path
     else
       redirect_to new_event_path
