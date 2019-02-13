5113fb971511add7a02f9fd8dddb0485a02b7bad
aurelien <duquenneaurelien@gmail.com>
2019-02-13 11:49:16 +0100

    profil user ok

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :verify_user, only: [:show]
 
   def index
   end
@@ -6,11 +8,31 @@ class UserController < ApplicationController
   def new
   end
 
+  def edit
+  end
+
   def show
-    @user = current_user.id
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(last_name: params[:last_name], first_name: params[:first_name], description: params[:description])
+      redirect_to root_path
+    else
+      redirect_to edit_user_path
+    end
   end
 
   def destroy
+  end
+
+private
+
+  def verify_user
+    unless current_user == User.find(params[:id])
+      redirect_to root_path
+  end
   end
 
 end
